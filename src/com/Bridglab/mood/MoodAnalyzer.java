836785cdb5b4088591c50e939c567078f12a7188
sonupooja <sonupooja2398@gmail.com>
2021-10-24 15:53:05 +0530

    Added:analysis sad

--- src/com/Bridglab/mood/MoodAnalyzer.java
+++ src/com/Bridglab/mood/MoodAnalyzer.java
@@ -1,19 +1,14 @@
 package com.Bridglab.mood;
 
 public class MoodAnalyzer {
-    private String message;
-
     public MoodAnalyzer() {
     }
 
-    public MoodAnalyzer(String message) {
-        this.message = message;
-    }
-
-    public String analyseMood() {
-        if (this.message.contains("Sad"))
+    public String MoodAnalyzerr(String message) {
+        if (message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
     }
 }
+
--- src/com/Bridglab/mood/MoodAnalyzerTest.java
+++ src/com/Bridglab/mood/MoodAnalyzerTest.java
@@ -5,17 +5,11 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_SadMood_Should_Return_Sad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
-        String mood = moodAnalyzer.analyseMood();
-        //Asserting The Result As Boolean Type True Or False
+    public void given_SadMood_Should_Return_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.MoodAnalyzerr("I am In a Sad Mood");
         Assert.assertEquals("SAD", mood);
+
     }
 
-    @Test
-    public void givenMessage_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY", mood);
-    }
 }
